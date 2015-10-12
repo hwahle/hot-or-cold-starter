b40198d1e105d1289fdcda1257e3ff20d330da7d
hwahle <helenwahle@gmail.com>
2015-10-12 15:39:19 -0700

    created hot and cold feature

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var newGameNumber = 0;
+var newGameNumber = 0,
+	guesses = [];
 
 
 $(document).ready(function(){
@@ -20,6 +21,10 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$("form").submit(function(evt) {
+  		evt.preventDefault();
+  		checkUserGuess();
+  	});
   	/*--- New Game ---*/
   	$("a.new").click(function(){
   		var newGame = location.reload();
@@ -27,5 +32,49 @@ $(document).ready(function(){
   	});
 
 });
+
+  	/*--- add to guess list ---*/
+  function addToList(blueArea) {
+  	guesses.push(blueArea);
+  	$("#count").text(guesses.length);
+  	$("ul#guessList").append("<li>" + blueArea + "</li>");
+  }
+/*--create write function for guess list---*/
+  	function write(txt) {
+  		document.getElementById("feedback").innerHTML = txt;
+  	}
+
+/*---checks if user guess is hot or cold---*/
+  	function checkUserGuess() {
+  		var userGuess = parseInt($("#userGuess").val()),
+  		theDifference = Math.abs(newGameNumber - userGuess);
+  		
+ 	
+ 	/*--- debug ---*/
+ 	console.log("newGameNumber:" + newGameNumber);
+ 	console.log("userGuess:" + userGuess);
+ 	console.log("theDifference:" + theDifference);
+
+ 	addToList(userGuess);
+
+ 	if (theDifference < 10)
+ 		write("On Fire");
+ 	else if (theDifference < 25)
+ 		write("Hotter");
+ 	else if (theDifference < 50)
+ 		write("Warm");
+ 	else if (theDifference < 75)
+ 		write("Cold");
+ 	else if (theDifference < 100)
+ 		write("Freezing");
+ 	else if (theDifference == 0)
+ 		write("Great work! You Guessed Correctly!");
+  					
+  		
+  	}
+  		
+
+
+
 
 
